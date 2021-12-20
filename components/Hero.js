fe28d05696bd08c86ea1020775b0c31050ae7446
Sahil Vasava <sahilvasava@gmail.com>
2021-12-21 00:33:45 +0530

    small design fixes

--- components/Hero.js
+++ components/Hero.js
@@ -6,11 +6,11 @@ function Hero() {
       <div className={` h-auto lg:min-h-[570px]  hero-gradient relative`}>
         <div className="text-center pt-20 sm:pt-24 w-4/6 mx-auto">
           <h2 className="font-Inter font-bold text-3xl lg:text-6xl text-white mb-3">
-            Learn to
+            <span>Learn to </span>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-hot-pink-g via-paris-daisy-g to-cyan-g">
-              &nbsp;code in Web3.&nbsp;
+              code in Web3.
             </span>
-            Build in less than 60s.
+            <span> Build in less than 60s.</span>
           </h2>
           <p className="font-Inter text-xl text-white text-opacity-75">
             Learning is always free. Join 15,000+ developers in learning web3,
