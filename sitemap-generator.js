9bc3b01f68f1383ab877a3ede9b76d8f560ec325
Sahil Vasava <sahilvasava@gmail.com>
2022-01-01 19:35:08 +0530

    added meta tags for SEO, robots.txt and a script to generate sitemap.xml

---
+++ sitemap-generator.js
@@ -0,0 +1,45 @@
+// import { configureSitemap } from '@sergeymyssak/nextjs-sitemap';
+const { configureSitemap } = require("@sergeymyssak/nextjs-sitemap");
+const tracks = require("./public/data/tracks.json");
+
+async function fetchDynamicPaths() {
+  let items = Object.keys(tracks).map(
+    (trackNameKey) => `/tracks/${trackNameKey}`
+  );
+  let paths = Object.values(tracks).map((track) =>
+    track?.quests.map((quest) => `/quest/${quest?.slug}`)
+  );
+  items = [].concat.apply(items, paths);
+  console.log(items);
+  return items;
+}
+
+async function getDynamicPaths() {
+  const paths = await fetchDynamicPaths();
+
+  return paths.map((item) => `/tracks/${item}`);
+}
+
+fetchDynamicPaths().then((paths) => {
+  const Sitemap = configureSitemap({
+    domains: [{ domain: "openquest.xyz", defaultLocale: "en" }],
+    include: paths,
+    exclude: ["/tracks/[trackNameKey]", "/quest/[questNameSlug]", "/api/*"],
+    excludeIndex: true,
+    pagesConfig: {
+      "/tracks/*": {
+        priority: "0.5",
+        changefreq: "daily",
+      },
+      "/quest/*": {
+        priority: "0.5",
+        changefreq: "daily",
+      },
+    },
+    trailingSlash: true,
+    targetDirectory: __dirname + "/public",
+    pagesDirectory: __dirname + "/pages",
+  });
+
+  Sitemap.generateSitemap();
+});
